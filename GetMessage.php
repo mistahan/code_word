e9c904999e8d67b444040765ca91a320c08431b4
Cesar Melchor <csrmelchor@gmail.com>
2016-12-05 12:40:11 -0800

    Update GetMessage.php

--- GetMessage.php
+++ GetMessage.php
@@ -1,26 +1,35 @@
 <?php
-    
+
     use \Firebase\JWT\JWT;
     define('SECRET_KEY','MFswDQYJKoZIhvcNAQEBBQADSgAwRwJARQqcj0UOTTzLoIGv2ljJVrvcz8CeAc/cUgFqo5gXwOo+2VHGvxz35f06GeyL4dYjTmuxquTrfikHNn+5Xc8brwIDAQAB'); /// secret key can be a random string and keep in secret from everyone
     define('ALGORITHM','HS256');
     require_once 'Dbconnect.php';
     require 'JWT.php';
-    
+
     header("Access-Control-Allow-Origin: *");
     header("Access-Control-Allow-Headers: Authorization, Origin, X-Requested-With, Content-Type,      Accept");
     header("Content-Type: application/json");
-    
+
     $headers = getallheaders();
     $jsonRecieved = $headers['Authorization'];
     $pieces = explode(" ", $jsonRecieved);
-    $token = $pieces[1];
-    
+    $token = $pieces[2];
     $secretK = base64_decode(SECRET_KEY);
-    $decoded_data_array = JWT::decode($token,$secretK,false);
-    
+
+
+    $decoded_data_array = JWT::decode($token,$secretK);
     $encodeArray = json_encode($decoded_data_array);
-    
+    $unencodedArray = ['information' => $decoded_data_array];
+    $data = $unencodedArray['information']->data;
+
+    $user = $data->username;
+
+    $messages = $DBcon->query("SELECT * FROM messages WHERE targetUser='$user' ORDER BY MessageTime ASC");
+
+    while ($row=mysqli_fetch_row($messages)) {
+      printf ("%s %s %s %s\n",$row[0],$row[1],$row[2],$row[3]);
+    }
     $DBcon->close();
 
-    
+
 ?>
